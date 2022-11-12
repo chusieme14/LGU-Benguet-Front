4a5bc6114695edb558379db36ff0fd47007377bb
Jomari Daryl Aviñon <avinonj@gmail.com>
2022-11-12 21:28:24 +0800

    partial program of work page

---
+++ pages/browse/program-of-work.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="browse-link-inner-cont">
+    <h2>
+      program of work
+    </h2>
+
+    <div class="program-insight">
+      <div class="item breakdown">
+        a
+      </div>
+      <div class="item equipment">
+        asd
+      </div>
+      <div class="item manpower">
+        fg
+      </div>
+    </div>
+
+    <div class="sign-valid">
+      <div class="sign-item">
+        <label>Prepared by:</label>
+        <h3>Jun L. Daliones</h3>
+        <span>Engineer I</span>
+        <img src="/images/sign.png" alt="sign" />
+      </div>
+      <div class="sign-item">
+        <label>Recommending Approval:</label>
+        <h3>Benedict P. Pineda</h3>
+        <span>Municipal Engineer</span>
+        <img src="/images/sign.png" alt="sign" />
+      </div>
+      <div class="sign-item">
+        <label>Approved:</label>
+        <h3>Romeo K. Salda</h3>
+        <span>Municipal Mayor</span>
+        <img src="/images/sign.png" alt="sign" />
+      </div>
+    </div>
+  </div>
+</template>
--- pages/browse/summary.vue
+++ pages/browse/summary.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="browse-link-inner-cont">
+    <h2>Summary</h2>
     <table>
       <tr class="header">
         <th>ITEM</th>
@@ -12,6 +13,31 @@
         <td>{{item.amount}}</td>
       </tr>
     </table>
+    <div class="summary-total">
+      <span>Sub - Total for A</span>
+      <span>P 265,846.62</span>
+    </div>
+
+    <div class="sign-valid">
+      <div class="sign-item">
+        <label>Prepared by:</label>
+        <h3>Jun L. Daliones</h3>
+        <span>Engineer I</span>
+        <img src="/images/sign.png" alt="sign" />
+      </div>
+      <div class="sign-item">
+        <label>Recommending Approval:</label>
+        <h3>Benedict P. Pineda</h3>
+        <span>Municipal Engineer</span>
+        <img src="/images/sign.png" alt="sign" />
+      </div>
+      <div class="sign-item">
+        <label>Approved:</label>
+        <h3>Romeo K. Salda</h3>
+        <span>Municipal Mayor</span>
+        <img src="/images/sign.png" alt="sign" />
+      </div>
+    </div>
   </div>
 </template>
 
