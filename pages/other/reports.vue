ce3de478ec4f17314e2a592268fa4b24ef4c0692
Jomari Daryl Aviñon <avinonj@gmail.com>
2022-12-10 21:17:02 +0800

    new pages

---
+++ pages/other/reports.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="browse-link-inner-cont dw-bg">
+    <h2>Reports</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  auth: false
+}
+</script>
---
+++ pages/other/unit-price-summary2.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="browse-link-inner-cont contractor-view-wrapper">
+    <h2>unit price summary asd</h2>
+    <div class="program-insight contractor-view">
+      <div class="item breakdown">
+        <table class="custom-table">
+          <tr class="header">
+            <th>Materials</th>
+            <th>unit</th>
+            <th>unit Price</th>
+          </tr>
+          <tr>
+            <td colspan="3">
+              I.
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div class="solid-td gray-text">
+                Material
+              </div>
+            </td>
+            <td class="pr-padding">
+              <div class="solid-td">
+                <select name="" id="">
+                  <option value="ft2">ft<sup>2</sup></option>
+                  <option value="ft2">ft<sup>2</sup></option>
+                </select>
+              </div>
+            </td>
+            <td>
+              <div class="solid-td gray-text">
+                P
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="3" class="t-padding">
+              <div class="dash-td txt-center gray-text">
+                Add another line
+              </div>
+            </td>
+          </tr>
+          <tr></tr>
+          <tr class="header">
+            <th>Labor</th>
+            <th>unit</th>
+            <th>unit Price</th>
+          </tr>
+          <tr>
+            <td colspan="3">
+              II.
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div class="solid-td gray-text">
+                Labor
+              </div>
+            </td>
+            <td class="pr-padding">
+              <div class="solid-td">
+                <select name="" id="">
+                  <option value="ft2">ft<sup>2</sup></option>
+                  <option value="ft2">ft<sup>2</sup></option>
+                </select>
+              </div>
+            </td>
+            <td>
+              <div class="solid-td gray-text">
+                P
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="3" class="t-padding">
+              <div class="dash-td txt-center gray-text">
+                Add another line
+              </div>
+            </td>
+          </tr>
+          <tr></tr>
+          <tr class="header">
+            <th>Equipemnt</th>
+            <th>unit</th>
+            <th>unit Price</th>
+          </tr>
+          <tr>
+            <td colspan="3">
+              II.
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div class="solid-td gray-text">
+                Labor
+              </div>
+            </td>
+            <td class="pr-padding">
+              <div class="solid-td">
+                <select name="" id="">
+                  <option value="1">Daily</option>
+                  <option value="2">Daily</option>
+                </select>
+              </div>
+            </td>
+            <td>
+              <div class="solid-td gray-text">
+                P
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="3" class="t-padding">
+              <div class="dash-td txt-center gray-text">
+                Add another line
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+
+    <div class="sign-valid">
+      <div class="sign-item">
+        <label>Prepared by:</label>
+        <h3>Jun L. Daliones</h3>
+        <span>Engineer I</span>
+        <img src="/images/sign.png" alt="sign" />
+      </div>
+      <div class="sign-item">
+        <label>Recommending Approval:</label>
+        <h3>Benedict P. Pineda</h3>
+        <span>Municipal Engineer</span>
+        <img src="/images/sign.png" alt="sign" />
+      </div>
+      <div class="sign-item">
+        <label>Approved:</label>
+        <h3>Romeo K. Salda</h3>
+        <span>Municipal Mayor</span>
+        <img src="/images/sign.png" alt="sign" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  auth: false,
+}
+</script>
